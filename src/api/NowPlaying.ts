62e1116b0314b117e13186561b038edbe2ecadd3
2lay <mail@2lay.net>
2024-12-28 16:29:23 +0100

    fix: nowplaying

--- src/api/NowPlaying.ts
+++ src/api/NowPlaying.ts
@@ -16,26 +16,29 @@ export const NowPlaying = new Elysia()
         return latestSong;
     });
 
-async function getLatestSong() {
-    const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USERNAME}&api_key=${process.env.LASTFM_API_KEY}&limit=1&format=json`;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+    async function getLatestSong() {
+        const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USERNAME}&api_key=${process.env.LASTFM_API_KEY}&limit=1&format=json`;
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            const track = json.recenttracks.track[0];
+            
+            return {
+                artist: track.artist['#text'],
+                name: track.name,
+                album: track.album['#text'],
+                url: track.url,
+                image: track.image.find((img: { size: string; }) => img.size === 'large')['#text'],
+                date: track['@attr']?.nowplaying ? 
+                    Math.floor(Date.now() / 1000) :
+                    Math.floor(new Date(track.date['#text']).getTime() / 1000),
+                isPlaying: Boolean(track['@attr']?.nowplaying)
+            };
+        } catch (error) {
+            console.error(error);
+            return { error: 'Failed to fetch the latest song' };
         }
-
-        const json = await response.json();
-        const track = json.recenttracks.track[0];
-        return {
-            artist: track.artist['#text'],
-            name: track.name,
-            album: track.album['#text'],
-            url: track.url,
-            image: track.image.find((img: { size: string; }) => img.size === 'large')['#text'],
-            date: Math.floor(new Date(track.date['#text']).getTime() / 1000)
-        };
-    } catch (error) {
-        console.error(error);
-        return { error: 'Failed to fetch the latest song' };
     }
-}
